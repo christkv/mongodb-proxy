ffea8c5165d2560420f37c1a11811255a77708a8
Christian Kvalheim <christkv@gmail.com>
2015-03-04 15:45:35 +0100

    Fixed correct handling of cursors when directing reads to secondaries

--- test/tests/connection_tests.js
+++ test/tests/connection_tests.js
@@ -82,3 +82,69 @@ exports['Concurrent cursors'] = {
     });
   }
 }
+
+exports['Should correctly connect to proxy and use readPreference secondary'] = {
+  metadata: { requires: { } },
+
+  // The actual test we wish to run
+  test: function(configuration, test) {
+    var MongoClient = require('mongodb').MongoClient;
+
+    // Url for connection to proxy
+    var url = 'mongodb://localhost:51000/test?readPreference=secondary';
+
+    // Connect to mongodb
+    MongoClient.connect(url, function(err, db) {
+      test.equal(null, err);
+
+      // Perform an inserts
+      db.collection('t3').insert([{a:1}, {b:1}, {c:1}, {d:1}], function(err, r) {
+        test.equal(null, err);
+
+        db.collection('t3').find({}).batchSize(2).toArray(function(err, docs) {
+          test.equal(null, err);
+
+          db.close();
+          test.done();
+        });
+      });
+    });
+  }
+}
+
+exports['Concurrent cursors against secondary'] = {
+  metadata: { requires: { } },
+
+  // The actual test we wish to run
+  test: function(configuration, test) {
+    var MongoClient = require('mongodb').MongoClient;
+
+    // Url for connection to proxy
+    var url = 'mongodb://localhost:51000/test?readPreference=secondary';
+
+    // Connect to mongodb
+    MongoClient.connect(url, function(err, db) {
+      test.equal(null, err);
+
+      // Perform an inserts
+      db.collection('t4').insert([{a:1}, {b:1}, {c:1}, {d:1}], {w:'majority'}, function(err, r) {
+        test.equal(null, err);
+
+        var total = 10;
+        var numberLeft = total;
+
+        for(var i = 0; i < total; i++) {
+          db.collection('t4').find({}).batchSize(2).toArray(function(err, docs) {
+            test.equal(null, err);
+            numberLeft = numberLeft - 1;
+
+            if(numberLeft == 0) {
+              db.close();
+              test.done();
+            }
+          });          
+        }
+      });
+    });
+  }
+}
